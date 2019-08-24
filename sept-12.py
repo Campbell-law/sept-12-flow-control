3fa63f5a4d8ad27514a81820d5b811fcd6e44425
tmb <tom.brooke@gmail.com>
2019-08-24 15:54:07 -0400

    final draft

---
+++ sept-12.py
@@ -0,0 +1,75 @@
+# Boolean values
+
+# 1). Is this valid:
+
+# True = 2+2
+
+#  What about these - are they True:
+
+2 + 2 == 4 
+2 + 2 == '4'
+
+
+#  Binary Operators
+# 2). What about these?
+
+2 + 3 == 5 and 4 + 5 == 9
+3 + 4 == 7 and 2 + 2 == 5
+3 + 4 == 7 or  2 + 2 == 5
+3 + 4 == 7 and not 2 + 2 == 5
+
+## Program flow
+## Python groups programs into blocks with indentation
+## If you are using the vscode interactive window you can highlight all the lines
+## you want to execute and press shift - return
+##
+
+name = 'Mary'
+password = 'crawdad'
+
+if name == 'Mary':
+    print("Hello " + name)
+    if password == 'crawdad':
+        print('Welcome ' + name)
+    else:
+        print('Sorry ' + name)
+
+# 3). Beer program - write a program that asks for a name and age
+## If the person is 21 or old they can buy beer if less than 21 they can't
+## To make the actual input work you have to run this in the terminal option command P on Mac 
+## or double click (right click) and select run in terminal -- 
+## or shift comand P and select Python run in terminal
+
+
+
+# 4) Grading program - if score is > 90 you get an A,
+##  > 80 < 90 you get a B
+##  > 70 < 80 a C
+##  > 60 < 70 a D
+## anything else you flunk
+
+
+
+# 5). Expungement App 
+## This is based on last years project. You can only expunge a criminal record under certain conditions
+## If you have one conviction you can usually get it expunged
+## if you have more than on conviction you can only get them expunged if they occurred on the same day
+
+
+# 6). Baseball App
+## Use a while loop to print strike 1, strike 2, strike 3, You're out
+
+# 7). Now use a for loop and do the same thing
+##  This one has a trick -- note that ranges start at 0
+
+
+# 8). Guessing game -- the book introduce modules and the function random()
+## Implement a guessing game that assigns a number between 1 and 10 and
+## the user has to guess the number
+## Hint you can use while True to keep a loop going then exit it with a break
+
+
+import random
+number = random.randint(1,10)
+
+   
